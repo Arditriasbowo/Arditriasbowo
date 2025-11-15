db3bac7613e988e3143f0468b432093b81ad7820
Arditriasbowo <138555991+Arditriasbowo@users.noreply.github.com>
2025-11-15 18:53:40 +0700

    fase 1

---
+++ script.js
@@ -0,0 +1,10 @@
+// toggle class activ
+const navBarNav = document.querySelector
+('.navbar-nav');
+
+// ketika humberger di klik
+
+document.querySelector
+('#hamburger-menu') ,onclick=()={
+    navBarNav,classlisttoggle('active');
+}
